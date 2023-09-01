fee5e2e3067aa197b93cde091c2f738ca6a98576
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2023-09-01 20:00:00 +0900

    Add files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Quick SUNY
+
+Chrome extension that provides quick access to SUNY Korea-related websites
+
+Inspired by [Quick Sook](https://github.com/seohyun0120/Quick-Sook)
+
+## Preview
+
+![Preview (light)](preview-light.png)
+
+![Preview (dark)](preview-dark.png)
+
+## Installation
+
+### Chrome Web Store
+
+[![Chrome Web Store](chrome-web-store.png)](https://chromewebstore.google.com/detail/quick-suny/ppkjeopklkmkgcmbjgfiacigjdmihcpe)
+
+### Manual Installation
+
+1. Download and unzip [Quick SUNY](https://github.com/yehwankim23/quick-suny/releases/latest/download/quick-suny.zip)
+2. Go to Chrome Extensions tab (`chrome://extensions`) and enable `Developer mode`
+3. Click `Load unpacked` and select the unzipped folder
---
+++ popup/data.js
@@ -0,0 +1,82 @@
+const data = [
+  [
+    {
+      name: "Brightspace",
+      icon: "../images/brightspace.png",
+      url: "https://mycourses.stonybrook.edu/d2l/home",
+    },
+    {
+      name: "Bulletin",
+      icon: "../images/bulletin.png",
+      url: "https://www.stonybrook.edu/sb/bulletin/current",
+    },
+    {
+      name: "Calendar",
+      icon: "../images/calendar.png",
+      url: "https://www.sunykorea.ac.kr/en/html/sub03/030501.html",
+    },
+    {
+      name: "Classie Evals",
+      icon: "../images/classie-evals.png",
+      url: "https://classie-evals.stonybrook.edu",
+    },
+    {
+      name: "Degree Works",
+      icon: "../images/degree-works.png",
+      url: "https://stonybrook.edu/degreeworks",
+    },
+    {
+      name: "SOLAR",
+      icon: "../images/solar.png",
+      url: "https://psns.cc.stonybrook.edu/psp/csprods/EMPLOYEE/CAMP/?cmd=login",
+    },
+  ],
+  [
+    { name: "AMS", icon: "../images/ams.png", url: "https://ams.sunykorea.ac.kr/ams/" },
+    { name: "BM", icon: "../images/bm.png", url: "https://bm.sunykorea.ac.kr/bm/" },
+    { name: "CS", icon: "../images/cs.png", url: "https://cs.sunykorea.ac.kr/cs/" },
+    { name: "ECE", icon: "../images/ece.png", url: "https://ece.sunykorea.ac.kr/ece/" },
+    { name: "ME", icon: "../images/me.png", url: "https://me.sunykorea.ac.kr/me/" },
+    { name: "TSM", icon: "../images/tsm.png", url: "https://ts.sunykorea.ac.kr/ts/" },
+  ],
+  [
+    { name: "FBM", icon: "../images/fbm.png", url: "https://fbm.sunykorea.ac.kr/fbm/" },
+    { name: "FD", icon: "../images/fd.png", url: "https://fd.sunykorea.ac.kr/fd/" },
+    {
+      name: "CDC",
+      icon: "../images/cdc.png",
+      url: "https://www.sunykorea.ac.kr/en/html/sub04/040401.html",
+    },
+    {
+      name: "Counseling",
+      icon: "../images/counseling.png",
+      url: "https://www.sunykorea.ac.kr/en/html/sub04/040202.html",
+    },
+    {
+      name: "Library",
+      icon: "../images/library.png",
+      url: "https://www.sunykorea.ac.kr/en/html/sub04/0405.html",
+    },
+    {
+      name: "Tutoring",
+      icon: "../images/tutoring.png",
+      url: "https://www.sunykorea.ac.kr/en/html/sub04/0406.html",
+    },
+  ],
+  [
+    { name: "IGC Housing", icon: "../images/igc-housing.png", url: "https://housing.igc.or.kr" },
+    {
+      name: "IGC IT Center",
+      icon: "../images/igc-it-center.png",
+      url: "https://itcenter.igc.or.kr",
+    },
+    { name: "IGC Library", icon: "../images/igc-library.png", url: "https://lib.igc.or.kr" },
+    { name: "Everytime", icon: "../images/everytime.png", url: "https://everytime.kr" },
+    { name: "Free Printer", icon: "../images/free-printer.png", url: "../free-printer.pdf" },
+    {
+      name: "Support",
+      icon: "../images/support.png",
+      url: "https://docs.google.com/forms/d/e/1FAIpQLSfcjZjkj-FtrXyvxr4GA-utyMcYvC5a1T2mhinoVuglPUV0Mw/viewform?usp=sf_link",
+    },
+  ],
+];
